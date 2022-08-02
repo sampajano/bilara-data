7a51db5b0f051bb30370a19e7caf572e8f1b770f
Hongda <hd2935@qq.com>
2022-08-02 20:11:47 +0800

    Update process_all_supported_files.py

--- .nilakkhana/process_all_supported_files.py
+++ .nilakkhana/process_all_supported_files.py
@@ -1,26 +1,23 @@
 import pathlib
 import json
 from typing import Dict
-
 from fast_nilakkhana import process_file
 
 repo_dir = pathlib.Path(__file__).absolute().parent.parent
 
-filtered_root_files = [x for x in repo_dir.glob('root/**/*.json') if x.name.find(
-    'site') == -1 and x.name.find('blurb') == -1 and x.name.find('name') == -1]
+root_files = [x for x in repo_dir.glob('root/**/*.json')]
 
-filtered_translation_files = [x for x in repo_dir.glob('translation/**/*.json') if x.name.find(
-    'site') == -1 and x.name.find('blurb') == -1 and x.name.find('name') == -1]
+translation_files = [x for x in repo_dir.glob('translation/**/*.json')]
 
 comment_files = [x for x in repo_dir.glob('comment/**/*.json')]
 
-print('Processing root text files[%d].'%(len(filtered_root_files)))
-for file in filtered_root_files:
+print('Processing root text files[%d].'%(len(root_files)))
+for file in root_files:
     process_file(file)
 print('Root text files transformed.')
 
-print('Processing translation text files[%d].'%(len(filtered_translation_files)))
-for file in filtered_translation_files:
+print('Processing translation text files[%d].'%(len(translation_files)))
+for file in translation_files:
     process_file(file)
 print('translation text files transformed.')
 
